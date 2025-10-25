44995f7f7098f808f281a14b9c4af0887a342096
Joshua <horheyinc@gmail.com>
2025-10-25 12:53:12 +0200

    day 3

--- module1/week2-joshua/day-3/index.js
+++ module1/week2-joshua/day-3/index.js
@@ -41,6 +41,52 @@ for (let i = 1; i <= 25; i += 1) {
 //for loop in reverse counting from 50 by 2s
 for (let i = 50; i >= 0; i -= 1) {
   if (i % 2 === 0) {
+    // console.log(i);
+  }
+}
+
+//FIZZ BUZZ Challenge
+//write a for loop that counts from 1 to 100
+// if the number is divisible by 3 .... then console.log('Fizz')
+// if the number is divisible by 5 .... then console.log('Buzz')
+// if the number is divisible by 3 and 5 .... then console.log('Fizz Buzz')
+//else you just log the number
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("Fizz Buzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+//****************while loop ***********/
+// let counter = 11;
+// while (counter <= 10) {
+//   console.log("while loop", counter);
+//   counter++;
+// }
+
+//***************do while loop */
+// let number = 0;
+// do {
+//   console.log("inside the do while", number);
+//   number++;
+// } while (number < 15);
+
+//for loop that counts to 20
+// if the number is 10, 11, or 12 then skip those numbers
+// if the number is 18 the break out the loop
+for (let i = 0; i <= 20; i++) {
+  if (i === 10 || i === 11 || i === 12) {
+    continue;
+  } else if (i === 19) {
+    break;
+  } else {
     console.log(i);
   }
 }
