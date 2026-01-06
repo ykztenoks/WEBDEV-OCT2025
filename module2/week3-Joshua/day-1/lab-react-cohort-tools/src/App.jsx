6b36393ca4f6ceff8bf35b3c2ba217dce2fee8ea
Joshua <horheyinc@gmail.com>
2026-01-06 11:54:39 -0600

    lab solution

---
+++ module2/week3-Joshua/day-1/lab-react-cohort-tools/src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import StudentDetailsPage from "./pages/StudentDetailsPage";
+import UserProfilePage from "./pages/UserProfilePage";
+
+function App() {
+  return (
+    <div className="App relative z-20 pt-20">
+      <Navbar />
+
+      <div className="pages">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/profile" element={<UserProfilePage />} />
+          <Route path="/students/:studentId" element={<StudentDetailsPage />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ module2/week3-Joshua/day-1/lab-react-cohort-tools/src/pages/HomePage.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import StudentCard from "../components/StudentCard";
+
+import studentsData from "../assets/students.json";
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  // eslint-disable-next-line no-unused-vars
+  const [students, setStudents] = useState(studentsData);
+
+  return (
+    <div className="border-2 border-rose-500 m-2">
+      <h1>Home Page</h1>
+      <div className="flex justify-between items-center p-2 font-bold border-b w-full">
+        <span
+          className="flex items-center justify-center"
+          style={{ flexBasis: "20%" }}
+        >
+          Image
+        </span>
+        <span style={{ flexBasis: "20%" }}>Name</span>
+        <span style={{ flexBasis: "20%" }}>Program</span>
+        <span style={{ flexBasis: "20%" }}>Email</span>
+        <span style={{ flexBasis: "20%" }}>Phone</span>
+      </div>
+
+      {students &&
+        students.map((student) => {
+          return (
+            <Link key={student._id} to={`/students/${student._id}`}>
+              <StudentCard {...student} />
+            </Link>
+          );
+        })}
+    </div>
+  );
+}
+
+export default HomePage;
