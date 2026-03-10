e81c3b3b38fbf7e1d46d26f32dc7ec2b796a7635
Joshua <horheyinc@gmail.com>
2026-03-10 15:42:56 -0500

    cors added

--- module3/week3-daniel/express-jwt-auth/app.js
+++ module3/week3-daniel/express-jwt-auth/app.js
@@ -1,22 +1,32 @@
-import express from "express"
-import * as dotenv from "dotenv"
-import { connectdb } from "./db/dbconnect.js"
-import authRouter from "./routes/auth.routes.js"
-import postRouter from "./routes/post.routes.js"
-import morgan from "morgan"
+import express from "express";
+import * as dotenv from "dotenv";
+import { connectdb } from "./db/dbconnect.js";
+import authRouter from "./routes/auth.routes.js";
+import postRouter from "./routes/post.routes.js";
+import morgan from "morgan";
+import cors from "cors";
 
-dotenv.config()
+const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(morgan("dev"))
+app.use(express.json());
+app.use(morgan("dev"));
+app.set("trust proxy", 1);
 
-app.use("/auth", authRouter)
-app.use("/posts", postRouter)
+// controls a very specific header to pass headers from the frontend
+app.use(
+  cors({
+    origin: [FRONTEND_URL],
+  }),
+);
+
+app.use("/auth", authRouter);
+app.use("/posts", postRouter);
 
 app.listen(process.env.PORT, () => {
-  console.clear()
-  console.log("Server up and running on port: " + process.env.PORT)
-  connectdb()
-})
+  console.clear();
+  console.log("Server up and running on port: " + process.env.PORT);
+  connectdb();
+});
